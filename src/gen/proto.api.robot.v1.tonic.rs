81a0abca18953acd2c82167669b098d63fe78cc4
viambot <viambot@users.noreply.github.com>
2022-07-25 20:11:19 +0000

    [WORKFLOW] Updating protos from viamrobotics/rdk, commit: da976e8e9f38fcb4606a1a32025510c8b76f6ac4

--- src/gen/proto.api.robot.v1.tonic.rs
+++ src/gen/proto.api.robot.v1.tonic.rs
@@ -255,6 +255,25 @@ pub mod robot_service_client {
             );
             self.inner.server_streaming(request.into_request(), path, codec).await
         }
+        pub async fn stop_all(
+            &mut self,
+            request: impl tonic::IntoRequest<super::StopAllRequest>,
+        ) -> Result<tonic::Response<super::StopAllResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/proto.api.robot.v1.RobotService/StopAll",
+            );
+            self.inner.unary(request.into_request(), path, codec).await
+        }
     }
 }
 /// Generated server implementations.
@@ -310,6 +329,10 @@ pub mod robot_service_server {
             &self,
             request: tonic::Request<super::StreamStatusRequest>,
         ) -> Result<tonic::Response<Self::StreamStatusStream>, tonic::Status>;
+        async fn stop_all(
+            &self,
+            request: tonic::Request<super::StopAllRequest>,
+        ) -> Result<tonic::Response<super::StopAllResponse>, tonic::Status>;
     }
     #[derive(Debug)]
     pub struct RobotServiceServer<T: RobotService> {
@@ -765,6 +788,44 @@ pub mod robot_service_server {
                                 send_compression_encodings,
                             );
                         let res = grpc.server_streaming(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/proto.api.robot.v1.RobotService/StopAll" => {
+                    #[allow(non_camel_case_types)]
+                    struct StopAllSvc<T: RobotService>(pub Arc<T>);
+                    impl<
+                        T: RobotService,
+                    > tonic::server::UnaryService<super::StopAllRequest>
+                    for StopAllSvc<T> {
+                        type Response = super::StopAllResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::StopAllRequest>,
+                        ) -> Self::Future {
+                            let inner = self.0.clone();
+                            let fut = async move { (*inner).stop_all(request).await };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = StopAllSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            );
+                        let res = grpc.unary(method, req).await;
                         Ok(res)
                     };
                     Box::pin(fut)
