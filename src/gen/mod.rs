5075dae0ec3c398fd327d45b8cfa4a5b1c0d913d
Ethan Rodkin <ethanrodkin@protonmail.com>
2022-09-21 10:17:07 -0400

    drop mod api from mod.rs

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -1,168 +1,166 @@
 pub mod proto {
-    pub mod api {
-        pub mod common {
-            // @@protoc_insertion_point(attribute:viam.common.v1)
+    pub mod common {
+        // @@protoc_insertion_point(attribute:viam.common.v1)
+        pub mod v1 {
+            include!("viam.common.v1.rs");
+            // @@protoc_insertion_point(viam.common.v1)
+        }
+    }
+    pub mod component {
+        pub mod arm {
+            // @@protoc_insertion_point(attribute:viam.component.arm.v1)
             pub mod v1 {
-                include!("viam.common.v1.rs");
-                // @@protoc_insertion_point(viam.common.v1)
+                include!("viam.component.arm.v1.rs");
+                // @@protoc_insertion_point(viam.component.arm.v1)
             }
         }
-        pub mod component {
-            pub mod arm {
-                // @@protoc_insertion_point(attribute:viam.component.arm.v1)
-                pub mod v1 {
-                    include!("viam.component.arm.v1.rs");
-                    // @@protoc_insertion_point(viam.component.arm.v1)
-                }
-            }
-            pub mod audioinput {
-                // @@protoc_insertion_point(attribute:viam.component.audioinput.v1)
-                pub mod v1 {
-                    include!("viam.component.audioinput.v1.rs");
-                    // @@protoc_insertion_point(viam.component.audioinput.v1)
-                }
-            }
-            pub mod base {
-                // @@protoc_insertion_point(attribute:viam.component.base.v1)
-                pub mod v1 {
-                    include!("viam.component.base.v1.rs");
-                    // @@protoc_insertion_point(viam.component.base.v1)
-                }
-            }
-            pub mod board {
-                // @@protoc_insertion_point(attribute:viam.component.board.v1)
-                pub mod v1 {
-                    include!("viam.component.board.v1.rs");
-                    // @@protoc_insertion_point(viam.component.board.v1)
-                }
-            }
-            pub mod camera {
-                // @@protoc_insertion_point(attribute:viam.component.camera.v1)
-                pub mod v1 {
-                    include!("viam.component.camera.v1.rs");
-                    // @@protoc_insertion_point(viam.component.camera.v1)
-                }
-            }
-            pub mod generic {
-                // @@protoc_insertion_point(attribute:viam.component.generic.v1)
-                pub mod v1 {
-                    include!("viam.component.generic.v1.rs");
-                    // @@protoc_insertion_point(viam.component.generic.v1)
-                }
-            }
-            pub mod gantry {
-                // @@protoc_insertion_point(attribute:viam.component.gantry.v1)
-                pub mod v1 {
-                    include!("viam.component.gantry.v1.rs");
-                    // @@protoc_insertion_point(viam.component.gantry.v1)
-                }
-            }
-            pub mod gripper {
-                // @@protoc_insertion_point(attribute:viam.component.gripper.v1)
-                pub mod v1 {
-                    include!("viam.component.gripper.v1.rs");
-                    // @@protoc_insertion_point(viam.component.gripper.v1)
-                }
-            }
-            pub mod inputcontroller {
-                // @@protoc_insertion_point(attribute:viam.component.inputcontroller.v1)
-                pub mod v1 {
-                    include!("viam.component.inputcontroller.v1.rs");
-                    // @@protoc_insertion_point(viam.component.inputcontroller.v1)
-                }
-            }
-            pub mod motor {
-                // @@protoc_insertion_point(attribute:viam.component.motor.v1)
-                pub mod v1 {
-                    include!("viam.component.motor.v1.rs");
-                    // @@protoc_insertion_point(viam.component.motor.v1)
-                }
-            }
-            pub mod movementsensor {
-                // @@protoc_insertion_point(attribute:viam.component.movementsensor.v1)
-                pub mod v1 {
-                    include!("viam.component.movementsensor.v1.rs");
-                    // @@protoc_insertion_point(viam.component.movementsensor.v1)
-                }
-            }
-            pub mod posetracker {
-                // @@protoc_insertion_point(attribute:viam.component.posetracker.v1)
-                pub mod v1 {
-                    include!("viam.component.posetracker.v1.rs");
-                    // @@protoc_insertion_point(viam.component.posetracker.v1)
-                }
-            }
-            pub mod sensor {
-                // @@protoc_insertion_point(attribute:viam.component.sensor.v1)
-                pub mod v1 {
-                    include!("viam.component.sensor.v1.rs");
-                    // @@protoc_insertion_point(viam.component.sensor.v1)
-                }
-            }
-            pub mod servo {
-                // @@protoc_insertion_point(attribute:viam.component.servo.v1)
-                pub mod v1 {
-                    include!("viam.component.servo.v1.rs");
-                    // @@protoc_insertion_point(viam.component.servo.v1)
-                }
-            }
-        }
-        pub mod robot {
-            // @@protoc_insertion_point(attribute:viam.robot.v1)
+        pub mod audioinput {
+            // @@protoc_insertion_point(attribute:viam.component.audioinput.v1)
             pub mod v1 {
-                include!("viam.robot.v1.rs");
-                // @@protoc_insertion_point(viam.robot.v1)
+                include!("viam.component.audioinput.v1.rs");
+                // @@protoc_insertion_point(viam.component.audioinput.v1)
             }
         }
-        pub mod service {
-            pub mod datamanager {
-                // @@protoc_insertion_point(attribute:viam.service.datamanager.v1)
-                pub mod v1 {
-                    include!("viam.service.datamanager.v1.rs");
-                    // @@protoc_insertion_point(viam.service.datamanager.v1)
-                }
+        pub mod base {
+            // @@protoc_insertion_point(attribute:viam.component.base.v1)
+            pub mod v1 {
+                include!("viam.component.base.v1.rs");
+                // @@protoc_insertion_point(viam.component.base.v1)
             }
-            pub mod shell {
-                // @@protoc_insertion_point(attribute:viam.service.shell.v1)
-                pub mod v1 {
-                    include!("viam.service.shell.v1.rs");
-                    // @@protoc_insertion_point(viam.service.shell.v1)
-                }
+        }
+        pub mod board {
+            // @@protoc_insertion_point(attribute:viam.component.board.v1)
+            pub mod v1 {
+                include!("viam.component.board.v1.rs");
+                // @@protoc_insertion_point(viam.component.board.v1)
             }
-            pub mod slam {
-                // @@protoc_insertion_point(attribute:viam.service.slam.v1)
-                pub mod v1 {
-                    include!("viam.service.slam.v1.rs");
-                    // @@protoc_insertion_point(viam.service.slam.v1)
-                }
+        }
+        pub mod camera {
+            // @@protoc_insertion_point(attribute:viam.component.camera.v1)
+            pub mod v1 {
+                include!("viam.component.camera.v1.rs");
+                // @@protoc_insertion_point(viam.component.camera.v1)
             }
-            pub mod motion {
-                // @@protoc_insertion_point(attribute:viam.service.motion.v1)
-                pub mod v1 {
-                    include!("viam.service.motion.v1.rs");
-                    // @@protoc_insertion_point(viam.service.motion.v1)
-                }
+        }
+        pub mod generic {
+            // @@protoc_insertion_point(attribute:viam.component.generic.v1)
+            pub mod v1 {
+                include!("viam.component.generic.v1.rs");
+                // @@protoc_insertion_point(viam.component.generic.v1)
             }
-            pub mod navigation {
-                // @@protoc_insertion_point(attribute:viam.service.navigation.v1)
-                pub mod v1 {
-                    include!("viam.service.navigation.v1.rs");
-                    // @@protoc_insertion_point(viam.service.navigation.v1)
-                }
+        }
+        pub mod gantry {
+            // @@protoc_insertion_point(attribute:viam.component.gantry.v1)
+            pub mod v1 {
+                include!("viam.component.gantry.v1.rs");
+                // @@protoc_insertion_point(viam.component.gantry.v1)
             }
-            pub mod sensors {
-                // @@protoc_insertion_point(attribute:viam.service.sensors.v1)
-                pub mod v1 {
-                    include!("viam.service.sensors.v1.rs");
-                    // @@protoc_insertion_point(viam.service.sensors.v1)
-                }
+        }
+        pub mod gripper {
+            // @@protoc_insertion_point(attribute:viam.component.gripper.v1)
+            pub mod v1 {
+                include!("viam.component.gripper.v1.rs");
+                // @@protoc_insertion_point(viam.component.gripper.v1)
             }
-            pub mod vision {
-                // @@protoc_insertion_point(attribute:viam.service.vision.v1)
-                pub mod v1 {
-                    include!("viam.service.vision.v1.rs");
-                    // @@protoc_insertion_point(viam.service.vision.v1)
-                }
+        }
+        pub mod inputcontroller {
+            // @@protoc_insertion_point(attribute:viam.component.inputcontroller.v1)
+            pub mod v1 {
+                include!("viam.component.inputcontroller.v1.rs");
+                // @@protoc_insertion_point(viam.component.inputcontroller.v1)
+            }
+        }
+        pub mod motor {
+            // @@protoc_insertion_point(attribute:viam.component.motor.v1)
+            pub mod v1 {
+                include!("viam.component.motor.v1.rs");
+                // @@protoc_insertion_point(viam.component.motor.v1)
+            }
+        }
+        pub mod movementsensor {
+            // @@protoc_insertion_point(attribute:viam.component.movementsensor.v1)
+            pub mod v1 {
+                include!("viam.component.movementsensor.v1.rs");
+                // @@protoc_insertion_point(viam.component.movementsensor.v1)
+            }
+        }
+        pub mod posetracker {
+            // @@protoc_insertion_point(attribute:viam.component.posetracker.v1)
+            pub mod v1 {
+                include!("viam.component.posetracker.v1.rs");
+                // @@protoc_insertion_point(viam.component.posetracker.v1)
+            }
+        }
+        pub mod sensor {
+            // @@protoc_insertion_point(attribute:viam.component.sensor.v1)
+            pub mod v1 {
+                include!("viam.component.sensor.v1.rs");
+                // @@protoc_insertion_point(viam.component.sensor.v1)
+            }
+        }
+        pub mod servo {
+            // @@protoc_insertion_point(attribute:viam.component.servo.v1)
+            pub mod v1 {
+                include!("viam.component.servo.v1.rs");
+                // @@protoc_insertion_point(viam.component.servo.v1)
+            }
+        }
+    }
+    pub mod robot {
+        // @@protoc_insertion_point(attribute:viam.robot.v1)
+        pub mod v1 {
+            include!("viam.robot.v1.rs");
+            // @@protoc_insertion_point(viam.robot.v1)
+        }
+    }
+    pub mod service {
+        pub mod datamanager {
+            // @@protoc_insertion_point(attribute:viam.service.datamanager.v1)
+            pub mod v1 {
+                include!("viam.service.datamanager.v1.rs");
+                // @@protoc_insertion_point(viam.service.datamanager.v1)
+            }
+        }
+        pub mod shell {
+            // @@protoc_insertion_point(attribute:viam.service.shell.v1)
+            pub mod v1 {
+                include!("viam.service.shell.v1.rs");
+                // @@protoc_insertion_point(viam.service.shell.v1)
+            }
+        }
+        pub mod slam {
+            // @@protoc_insertion_point(attribute:viam.service.slam.v1)
+            pub mod v1 {
+                include!("viam.service.slam.v1.rs");
+                // @@protoc_insertion_point(viam.service.slam.v1)
+            }
+        }
+        pub mod motion {
+            // @@protoc_insertion_point(attribute:viam.service.motion.v1)
+            pub mod v1 {
+                include!("viam.service.motion.v1.rs");
+                // @@protoc_insertion_point(viam.service.motion.v1)
+            }
+        }
+        pub mod navigation {
+            // @@protoc_insertion_point(attribute:viam.service.navigation.v1)
+            pub mod v1 {
+                include!("viam.service.navigation.v1.rs");
+                // @@protoc_insertion_point(viam.service.navigation.v1)
+            }
+        }
+        pub mod sensors {
+            // @@protoc_insertion_point(attribute:viam.service.sensors.v1)
+            pub mod v1 {
+                include!("viam.service.sensors.v1.rs");
+                // @@protoc_insertion_point(viam.service.sensors.v1)
+            }
+        }
+        pub mod vision {
+            // @@protoc_insertion_point(attribute:viam.service.vision.v1)
+            pub mod v1 {
+                include!("viam.service.vision.v1.rs");
+                // @@protoc_insertion_point(viam.service.vision.v1)
             }
         }
     }
